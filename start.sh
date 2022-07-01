043454db846330bdc9a7bbcb5efd6efcdec6c3f1
Avinash-Palacharla <107996709+Avinash-Palacharla@users.noreply.github.com>
2022-07-01 16:09:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,14 +3,38 @@
 
 
 
-mysql -uroot -p -e "create table project.project_sql(custid integer(10) primary key not null,username varchar(30),quote_count varchar(30),ip varchar(30),entry_time varchar(30),prp_1 varchar(30),prp_2 varchar(30),prp_3 varchar(30),ms varchar(30),http_type varchar(30),purchase_category varchar(30),total_count varchar(30),purchase_sub_category varchar(30),http_info varchar(30),status_code integer(10),curr_time bigint);"
+mysql -uroot -p -e "create table project.project_sql(
+  custid integer(10) primary key not null,
+  username varchar(30),
+  quote_count varchar(30),
+  ip varchar(30),
+  entry_time varchar(30),
+  prp_1 varchar(30),
+  prp_2 varchar(30),
+  prp_3 varchar(30),
+  ms varchar(30),
+  http_type varchar(30),
+  purchase_category varchar(30),
+  total_count varchar(30),
+  purchase_sub_category varchar(30),
+  http_info varchar(30),
+  status_code integer(10),
+  curr_time bigint
+  );"
 
 mysql --local-infile=1 -uroot -pWelcome@123 -e "set global local_infile=1;
+
 load data local infile '/home/saif/LFS/FF11/project/Day_1.csv' into table project.project_sql fields terminated by ',';
+
 update project.project_sql set curr_time = CURRENT_TIMESTAMP() + 1 where curr_time IS NULL;
 "
 
-sqoop import --connect jdbc:mysql://localhost:3306/project?useSSL=False --username root --password Welcome@123 --query 'select custid,username,quote_count,ip,entry_time,prp_1,prp_2,prp_3,ms,http_type,purchase_category,total_count,purchase_sub_category,http_info,status_code,curr_time from project_sql where $CONDITIONS' --split-by custid --target-dir /user/saif/HFS/output/project_1;
+sqoop import \
+--connect jdbc:mysql://localhost:3306/project?useSSL=False \
+--username root --password Welcome@123 \
+--query 'select custid,username,quote_count,ip,entry_time,prp_1,prp_2,prp_3,ms,http_type,purchase_category,total_count,purchase_sub_category,http_info,status_code,curr_time from project_sql where $CONDITIONS' \
+--split-by custid \
+--target-dir /user/saif/HFS/output/project_1;
 
 
 hive -e "
@@ -87,7 +111,31 @@ row format delimited fields terminated by ',';
 insert into table project_hive.project_inter select * from project_hive.project_int_par t1 join (select max(curr_time) as max_date_time from project_hive.project_int_par) tt1 on tt1.max_date_time = t1.curr_time;"
 
 
-mysql -uroot -p -e "create table project.project_sql_exp (custid integer(10),username varchar(30),quote_count varchar(30),ip varchar(30),entry_time varchar(30),prp_1 varchar(30),prp_2 varchar(30),prp_3 varchar(30),ms varchar(30),http_type varchar(30),purchase_category varchar(30),total_count varchar(30),purchase_sub_category varchar(30),http_info varchar(30),status_code integer(10),year varchar(100),month varchar(100),curr_time bigint);"
+mysql -uroot -p -e "create table project.project_sql_exp (
+custid integer(10),
+username varchar(30),
+quote_count varchar(30),
+ip varchar(30),
+entry_time varchar(30),
+prp_1 varchar(30),
+prp_2 varchar(30),
+prp_3 varchar(30),
+ms varchar(30),
+http_type varchar(30),
+purchase_category varchar(30),
+total_count varchar(30),
+purchase_sub_category varchar(30),
+http_info varchar(30),
+status_code integer(10),
+year varchar(100),
+month varchar(100),
+curr_time bigint
+);"
 
 
-sqoop export --connect jdbc:mysql://localhost:3306/project?useSSL=False --table project_sql_exp --username root --password Welcome@123 --export-dir "/user/hive/warehouse/project_hive.db/project_inter" --input-fields-terminated-by ',';
+sqoop export \
+--connect jdbc:mysql://localhost:3306/project?useSSL=False \
+--table project_sql_exp \
+--username root --password Welcome@123 \
+--export-dir "/user/hive/warehouse/project_hive.db/project_inter" \
+--input-fields-terminated-by ',';
